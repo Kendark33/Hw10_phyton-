6af7b7f8e1ef4a1560b979e0dc83d0080004c6a6
Kendark33 <you@example.com>
2023-04-11 09:41:51 +0300

    Output, search

---
+++ output.py
@@ -0,0 +1,15 @@
+def OutputAll():
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line)
+
+
+def Search(data):
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        flag = False
+        for line in file:
+            if data in line.lower():
+                print(line)
+                flag = True
+        if flag == False:
+            print('\nНе найдено! \n')
